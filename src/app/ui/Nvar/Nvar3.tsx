74d99af11094b756e83044f0b31253ac41f2100e
JSalinas13 <jesus.salinas.le.9@gmail.com>
2024-05-23 13:27:01 -0600

    se acomodo la barra de navegacion

--- src/app/ui/Nvar/Nvar3.tsx
+++ src/app/ui/Nvar/Nvar3.tsx
@@ -56,22 +56,19 @@ const Nvar2: React.FC<NavbarProps> = ({ onIconClick }) => {
                             Tienda
                         </a>
                     </li>
+                    <li>
+                        <a className="block hover:text-gray-200" href="/pages/auth/registro">
+                            Registro
+                        </a>
+                    </li>
+                    <li>
+                        <a className="block hover:text-gray-200" href="/pages/auth/login">
+                            Iniciar sesion
+                        </a>
+                    </li>
       
                 </ul>
-                <button className="flex items-center px-4 py-2 space-x-2" onClick={handlerStore}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#fff">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                    </svg>
-                    <span>Tienda</span>
-                </button>
-                <button className="flex items-center px-4 py-2 space-x-2" onClick={handlerLogin}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512" style={{ fill: 'none', stroke: '#fff', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '32px' }}>
-                        <path d="M320,176V136a40,40,0,0,0-40-40H88a40,40,0,0,0-40,40V376a40,40,0,0,0,40,40H280a40,40,0,0,0,40-40V336" />
-                        <polyline points="384 176 464 256 384 336" />
-                        <line x1="191" y1="256" x2="464" y2="256" />
-                    </svg>
-                    <span>Iniciar Sesion</span>
-                </button>
+               
                 {/* <button className="flex items-center px-4 py-2 space-x-2" >
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6a4 4 0 100 8 4 4 0 000-8z" />
